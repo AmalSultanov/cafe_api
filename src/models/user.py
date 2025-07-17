d74228336a957a951c3fd9d70d8a7844882f064b
AmalSultanov <pogboom1014@gmail.com>
2025-07-17 20:48:22 +0500

    Add users table

---
+++ src/models/user.py
@@ -0,0 +1,90 @@
+import enum
+from datetime import datetime
+
+from sqlalchemy import (
+    ForeignKey, UniqueConstraint, Enum, Integer, String, DateTime
+)
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+
+from src.database import Base
+
+
+class IdentityProviderEnum(enum.Enum):
+    telegram = "telegram"
+    web = "web"
+
+
+class UserModel(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True
+    )
+    name: Mapped[str | None] = mapped_column(String, nullable=True)
+    surname: Mapped[str | None] = mapped_column(String, nullable=True)
+    phone_number: Mapped[str] = mapped_column(
+        String, unique=True, nullable=False
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.utcnow
+    )
+
+    identities: Mapped[list["UserIdentityModel"]] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
+
+    repr_cols_num = 2
+    repr_cols = ("created_at",)
+
+    def __str__(self):
+        return self.name
+
+
+class UserIdentityModel(Base):
+    __tablename__ = "user_identities"
+    __table_args__ = ((
+        UniqueConstraint(
+            "provider", "provider_id", name="uq_provider_provider_id"
+        )),
+    )
+
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True
+    )
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+    provider: Mapped[IdentityProviderEnum] = mapped_column(
+        Enum(
+            IdentityProviderEnum,
+            name="identityproviderenum",
+            create_type=False
+        )
+    )
+    provider_id: Mapped[str] = mapped_column(String, nullable=False)
+    username: Mapped[str | None] = mapped_column(String, nullable=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.utcnow
+    )
+
+    user: Mapped[UserModel] = relationship(back_populates="identities")
+
+    repr_cols_num = 2
+    repr_cols = ("created_at",)
+
+    def __str__(self):
+        return f"{self.provider} user with provider_id={self.provider_id}"
+
+
+# class TelegramIdentityModel(Base):
+#     __tablename__ = "telegram_identities"
+#
+#     id: Mapped[int] = mapped_column(
+#         Integer, primary_key=True, autoincrement=True
+#     )
+#     identity_id: Mapped[int] = mapped_column(
+#         ForeignKey("user_identities.id", ondelete="CASCADE"), unique=True
+#     )
+#     telegram_username: Mapped[str | None] = mapped_column(String(50))
+#
+#     identity: Mapped[UserIdentityModel] = relationship(backref="telegram_identity")
