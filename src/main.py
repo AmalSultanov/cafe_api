5f27b7100103d9fb56dbb6df7982646b1d8d8487
AmalSultanov <pogboom1014@gmail.com>
2025-07-18 00:05:11 +0500

    Add main file as the entry point

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
 
-from src.meals.router import meals_router
-from src.users.router import users_router
+from src.core.constants import APP_SUMMARY, APP_DESCRIPTION, APP_VERSION
+from src.controllers.v1 import api_v1_router
 
 app = FastAPI(
-    title='CafeAPI',
-    summary='A set of API endpoints designed for '
-            'integration with external services.',
-    docs_url='/docs',
-    redoc_url='/redoc'
+    title="CafeAPI",
+    summary=APP_SUMMARY,
+    description=APP_DESCRIPTION,
+    version=APP_VERSION,
+    docs_url="/docs",
+    redoc_url="/redoc"
 )
-app.include_router(users_router)
-app.include_router(meals_router)
+app.include_router(api_v1_router, prefix="/api")
