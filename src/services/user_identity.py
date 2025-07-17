50cd54c2e7c06c6f328f00a5baaab6df16b71c77
AmalSultanov <pogboom1014@gmail.com>
2025-07-17 21:14:22 +0500

    Add user identity service

---
+++ src/services/user_identity.py
@@ -0,0 +1,21 @@
+from src.repositories.user.identity_interface import IUserIdentityRepository
+from src.schemas.user import IdentityCheck
+
+
+class UserIdentityService:
+    def __init__(self, user_identity_repo: IUserIdentityRepository):
+        self.user_identity_repo = user_identity_repo
+
+    async def create_identity(
+         self, user_id: int, identity_data: IdentityCheck
+    ):
+        await self.user_identity_repo.create(
+            user_id, identity_data.model_dump()
+        )
+
+    async def is_registered(self, identity_data: IdentityCheck) -> bool:
+        return (
+            await self.user_identity_repo.get_by_provider(
+                identity_data.model_dump()
+            ) is not None
+        )
