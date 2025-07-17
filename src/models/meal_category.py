fafbc02e0b22c589f7c629561755e758f215898b
AmalSultanov <pogboom1014@gmail.com>
2025-07-17 20:47:45 +0500

    Add meal_categories table

---
+++ src/models/meal_category.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+
+from src.database import Base
+
+
+class MealCategoryModel(Base):
+    __tablename__ = "meal_categories"
+
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True
+    )
+    name: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+
+    meals: Mapped["MealModel"] = relationship(
+        "MealModel", back_populates="category"
+    )
+
+    repr_cols_num = 2
+    repr_cols = ("created_at",)
+
+    def __str__(self):
+        return self.name
