817b5634e7481623061f6e7c4f922627a0bd1dd9
AmalSultanov <pogboom1014@gmail.com>
2025-07-23 18:14:06 +0500

    Add pydantic schemas for cart_item

---
+++ src/schemas/cart_item.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+from decimal import Decimal
+
+from pydantic import BaseModel, Field, ConfigDict, field_validator
+
+from src.exceptions.cart_item import CartItemQuantityError
+
+
+class CartItemBase(BaseModel):
+    meal_id: int
+    quantity: int
+
+
+class CartItemCreate(CartItemBase):
+    @field_validator("quantity")
+    @classmethod
+    def validate_quantity(cls, value):
+        if value is not None and value <= 0:
+            raise CartItemQuantityError()
+        return value
+
+
+class CartItemPutUpdate(CartItemBase):
+    pass
+
+
+class CartItemPatchUpdate(BaseModel):
+    quantity: int | None = None
+
+    @field_validator("quantity")
+    @classmethod
+    def validate_quantity(cls, v):
+        if v is not None and v <= 0:
+            raise CartItemQuantityError()
+        return v
+
+
+class CartItemRead(CartItemBase):
+    id: int
+    meal_name: str
+    unit_price: Decimal
+    total_price: Decimal
+    created_at: datetime
+
+    model_config = ConfigDict(json_encoders={
+        Decimal: lambda v: format(v, "f")
+    })
