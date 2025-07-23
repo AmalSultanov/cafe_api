5e8d571144f5c9350029f6ce9543820c656d4410
AmalSultanov <pogboom1014@gmail.com>
2025-07-23 18:35:13 +0500

    Add DI for cart_item

---
+++ src/core/dependencies/cart_item.py
@@ -0,0 +1,26 @@
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.core.database import get_session
+from src.core.dependencies.cart import get_cart_service
+from src.core.dependencies.meal import get_meal_service
+from src.repositories.cart_item.interface import ICartItemRepository
+from src.repositories.cart_item.repository import CartItemRepository
+from src.services.cart.interface import ICartService
+from src.services.cart_item.interface import ICartItemService
+from src.services.cart_item.service import CartItemService
+from src.services.meal.interface import IMealService
+
+
+def get_cart_item_repo(
+    db: AsyncSession = Depends(get_session)
+) -> ICartItemRepository:
+    return CartItemRepository(db)
+
+
+def get_cart_item_service(
+    repository: ICartItemRepository = Depends(get_cart_item_repo),
+    cart_service: ICartService = Depends(get_cart_service),
+    meal_service: IMealService = Depends(get_meal_service)
+) -> ICartItemService:
+    return CartItemService(repository, cart_service, meal_service)
