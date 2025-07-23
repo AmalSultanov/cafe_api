b0e7270370dbf88ff78c417a5ee28547c678ea68
AmalSultanov <pogboom1014@gmail.com>
2025-07-23 18:30:54 +0500

    Add user and user_identity related exceptions

--- src/exceptions/user.py
+++ src/exceptions/user.py
@@ -1,11 +1,39 @@
-from src.exceptions.base import ConflictError, NotFoundError
+from src.exceptions.base import ConflictError, NotFoundError, ValidationError
 
 
 class UserAlreadyExistsError(ConflictError):
-    def __init__(self, name: str):
-        super().__init__(f"User with name '{name}' already exists")
+    def __init__(self, username: str, provider: str):
+        super().__init__(
+            f"User with username '{username}' from {provider} already exists"
+        )
+
+
+class UserPhoneAlreadyExistsError(ConflictError):
+    def __init__(self, phone_number: str):
+        super().__init__(
+            f"User with phone number +'{phone_number}' already exists"
+        )
 
 
 class UserNotFoundError(NotFoundError):
     def __init__(self, user_id: int):
         super().__init__(f"User with ID={user_id} was not found")
+
+
+class NoUserUpdateDataError(ValidationError):
+    def __init__(self):
+        super().__init__("No data provided for update")
+
+
+class UserIdentityAlreadyExistsError(ConflictError):
+    def __init__(self, provider_id: str, provider: str):
+        super().__init__(
+            f"User with provider ID={provider_id} from {provider} already exists"
+        )
+
+
+class UserIdentityNotFoundError(NotFoundError):
+    def __init__(self, provider_id: str, provider: str):
+        super().__init__(
+            f"User with provider ID={provider_id} from {provider} was not found"
+        )
