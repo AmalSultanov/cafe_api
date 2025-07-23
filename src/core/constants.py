fd53198065ef52c3810154c507f6d779546d31c6
AmalSultanov <pogboom1014@gmail.com>
2025-07-23 18:47:39 +0500

    Add constants

---
+++ src/core/constants.py
@@ -0,0 +1,9 @@
+APP_SUMMARY = """
+Cafe Management and Ordering API
+"""
+APP_DESCRIPTION = """
+Provides a backend for managing users, meals, categories, and user carts.
+Supports user registration and authentication via Telegram and web clients.
+Enables meal browsing, cart operations, and order placement through a unified API.
+"""
+APP_VERSION = "1.0.0"
