530da2ba4d7a47da96b03dd58d0ed0f8a55b0b01
AmalSultanov <pogboom1014@gmail.com>
2025-07-23 18:24:35 +0500

    Create MealService

--- src/services/meal/service.py
+++ src/services/meal/service.py
@@ -1,28 +1,31 @@
+from sqlalchemy.exc import IntegrityError
+
 from src.core.utils import url_to_str
-from src.exceptions.meal import MealNotFoundError, MealAlreadyExistsError
+from src.exceptions.meal import (
+    MealNotFoundError, MealAlreadyExistsError, NoMealUpdateDataError
+)
 from src.exceptions.meal_category import MealCategoryNotFoundError
-from src.models.meal import MealModel
 from src.repositories.meal.interface import IMealRepository
-from src.repositories.meal_category.interface import IMealCategoryRepository
-from src.schemas.meal import MealCreate, MealUpdate, MealPutUpdate
+from src.schemas.meal import MealCreate, MealUpdate, MealPutUpdate, MealRead
+from src.services.meal_category.interface import IMealCategoryService
 
 
 class MealService:
     def __init__(
         self,
-        meal_repo: IMealRepository,
-        category_repo: IMealCategoryRepository
-    ):
-        self.meal_repo = meal_repo
-        self.category_repo = category_repo
+        repository: IMealRepository,
+        category_service: IMealCategoryService
+    ) -> None:
+        self.repository = repository
+        self.category_service = category_service
 
     async def create_meal(
         self, category_id: int, meal_data: MealCreate
-    ) -> MealModel:
-        if not await self.category_repo.get_by_id(category_id):
+    ) -> MealRead:
+        if not await self.category_service.get_category(category_id):
             raise MealCategoryNotFoundError(category_id)
 
-        if await self.meal_repo.get_by_name(meal_data.name):
+        if await self.repository.get_by_name(meal_data.name):
             raise MealAlreadyExistsError(meal_data.name)
 
         if meal_data.image_url:
@@ -31,45 +34,54 @@ class MealService:
         meal_dict = meal_data.model_dump()
         meal_dict["category_id"] = category_id
 
-        return await self.meal_repo.create(meal_dict)
+        try:
+            meal = await self.repository.create(meal_dict)
+        except IntegrityError:
+            raise MealAlreadyExistsError(meal_dict["name"])
+        return MealRead.model_validate(meal)
 
     async def get_meals_by_category_id(
         self, category_id: int
-    ) -> list[MealModel]:
-        if not await self.category_repo.get_by_id(category_id):
+    ) -> list[MealRead]:
+        if not await self.category_service.get_category(category_id):
             raise MealCategoryNotFoundError(category_id)
 
-        return await self.meal_repo.get_all_by_category_id(category_id)
+        meals = await self.repository.get_all_by_category_id(category_id)
+        return [MealRead.model_validate(meal) for meal in meals]
 
-    async def get_meal(self, category_id: int, meal_id: int) -> MealModel:
-        if not await self.category_repo.get_by_id(category_id):
-            raise MealCategoryNotFoundError(category_id)
-
-        meal = await self.meal_repo.get_by_id(meal_id)
-
-        if not meal or meal.category_id != category_id:
-            raise MealNotFoundError(meal_id)
-        return meal
-
-    async def get_meal_by_id(self, meal_id: int) -> MealModel:
-        meal = await self.meal_repo.get_by_id(meal_id)
+    async def get_meal(
+        self, meal_id: int, category_id: int = None
+    ) -> MealRead:
+        meal = await self.repository.get_by_id(meal_id)
 
         if not meal:
             raise MealNotFoundError(meal_id)
 
-        return meal
+        if category_id is not None and meal.category_id != category_id:
+            if not await self.category_service.get_category(category_id):
+                raise MealCategoryNotFoundError(category_id)
+            raise MealNotFoundError(meal_id)
+
+        return MealRead.model_validate(meal)
 
     async def update_meal(
         self,
         category_id: int,
         meal_id: int,
         meal_data: MealPutUpdate | MealUpdate,
-        is_partial: bool = False,
-    ) -> MealModel:
-        if not await self.category_repo.get_by_id(category_id):
+        is_partial: bool = False
+    ) -> MealRead:
+        new_data = (
+            meal_data.model_dump(exclude_unset=True) if is_partial
+            else meal_data.model_dump()
+        )
+
+        if not new_data:
+            raise NoMealUpdateDataError()
+        if not await self.category_service.get_category(category_id):
             raise MealCategoryNotFoundError(category_id)
 
-        meal = await self.meal_repo.get_by_id(meal_id)
+        meal = await self.repository.get_by_id(meal_id)
 
         if not meal or meal.category_id != category_id:
             raise MealNotFoundError(meal_id)
@@ -77,27 +89,25 @@ class MealService:
         if hasattr(meal_data, "image_url") and meal_data.image_url is not None:
             meal_data.image_url = url_to_str(meal_data.image_url)
 
-        data = (
-            meal_data.model_dump(exclude_unset=True)
-            if is_partial
-            else meal_data.model_dump()
-        )
-
-        if "name" in data:
-            existing = await self.meal_repo.get_by_name(data["name"])
+        if "name" in new_data:
+            existing = await self.repository.get_by_name(new_data["name"])
 
             if existing and existing.id != meal_id:
-                raise MealAlreadyExistsError(data["name"])
+                raise MealAlreadyExistsError(new_data["name"])
 
-        return await self.meal_repo.update(meal_id, data)
+        try:
+            upd_meal = await self.repository.update(meal_id, new_data)
+        except IntegrityError:
+            raise MealAlreadyExistsError(new_data["name"])
+        return MealRead.model_validate(upd_meal)
 
     async def delete_meal(self, category_id: int, meal_id: int) -> None:
-        if not await self.category_repo.get_by_id(category_id):
+        if not await self.category_service.get_category(category_id):
             raise MealCategoryNotFoundError(category_id)
 
-        meal = await self.meal_repo.get_by_id(meal_id)
+        meal = await self.repository.get_by_id(meal_id)
 
         if not meal or meal.category_id != category_id:
             raise MealNotFoundError(meal_id)
 
-        return await self.meal_repo.delete(meal_id)
+        return await self.repository.delete(meal_id)
