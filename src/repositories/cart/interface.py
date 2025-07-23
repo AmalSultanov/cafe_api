93f33f225346c9618a86603690ddae2de17202d3
AmalSultanov <pogboom1014@gmail.com>
2025-07-23 18:02:31 +0500

    Create repository interfaces for cart and cart item

---
+++ src/repositories/cart/interface.py
@@ -0,0 +1,14 @@
+from typing import Protocol
+
+from src.models.cart import CartModel
+
+
+class ICartRepository(Protocol):
+    async def create(self, user_id: int) -> CartModel:
+        ...
+
+    async def get_by_user_id(self, user_id: int) -> CartModel | None:
+        ...
+
+    async def delete(self, cart_id: int) -> None:
+        ...
