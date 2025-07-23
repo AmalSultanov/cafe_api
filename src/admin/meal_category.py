9095588332b55101465e939a507feef267a4b6e9
AmalSultanov <pogboom1014@gmail.com>
2025-07-23 23:51:05 +0500

    Add MealCategoryAdmin

---
+++ src/admin/meal_category.py
@@ -0,0 +1,10 @@
+from fastapi_amis_admin.admin import admin
+
+from src.admin.site import site
+from src.models.meal_category import MealCategoryModel
+
+
+@site.register_admin
+class MealCategoryAdmin(admin.ModelAdmin):
+    page_schema = "MealCategoryModel"
+    model = MealCategoryModel
