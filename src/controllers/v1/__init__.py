12d4804243ce123e520130b055dac1a0a4ae4160
AmalSultanov <pogboom1014@gmail.com>
2025-07-23 18:42:46 +0500

    Register routers for cart and cart_items

--- src/controllers/v1/__init__.py
+++ src/controllers/v1/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
-from src.controllers.v1 import users, meals, meal_categories
+from src.controllers.v1 import users, meals, meal_categories, cart, cart_items
 
 api_v1_router = APIRouter(prefix="/v1")
 
-api_v1_router.include_router(users.router)
-api_v1_router.include_router(meals.router)
 api_v1_router.include_router(meal_categories.router)
+api_v1_router.include_router(meals.router)
+api_v1_router.include_router(users.router)
+api_v1_router.include_router(cart.router)
+api_v1_router.include_router(cart_items.router)
