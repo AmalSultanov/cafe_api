b0694ecd432f884d6b267a53d3d22e25a98556a3
AmalSultanov <pogboom1014@gmail.com>
2025-07-23 23:50:31 +0500

    Add CartItemAdmin

---
+++ src/admin/cart_item.py
@@ -0,0 +1,10 @@
+from fastapi_amis_admin.admin import admin
+
+from src.admin.site import site
+from src.models.cart_item import CartItemModel
+
+
+@site.register_admin
+class CartItemAdmin(admin.ModelAdmin):
+    page_schema = "CartItemModel"
+    model = CartItemModel
