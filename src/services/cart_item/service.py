0b87bb9870f826ec5aa05b710923f47de198c09b
AmalSultanov <pogboom1014@gmail.com>
2025-07-24 20:07:56 +0500

    Add cart_item dict construction to add_item_to_cart, update signature of update service method

--- src/services/cart_item/service.py
+++ src/services/cart_item/service.py
@@ -5,7 +5,7 @@ from src.exceptions.cart_item import (
 from src.models.cart_item import CartItemModel
 from src.repositories.cart_item.interface import ICartItemRepository
 from src.schemas.cart_item import (
-    CartItemCreate, CartItemPatchUpdate, CartItemRead
+    CartItemCreate, CartItemPatchUpdate, CartItemRead, CartItemPutUpdate
 )
 from src.services.cart.interface import ICartService
 from src.services.meal.interface import IMealService
@@ -22,12 +22,12 @@ class CartItemService:
         self.cart_service = cart_service
         self.meal_service = meal_service
 
-    async def _to_cart_item_read(self, item: CartItemModel) -> CartItemRead:
-        meal = await self.meal_service.get_meal(item.meal_id)
+    @staticmethod
+    def _to_cart_item_read(item: CartItemModel) -> CartItemRead:
         return CartItemRead(
             id=item.id,
             meal_id=item.meal_id,
-            meal_name=meal.name,
+            meal_name=item.meal_name,
             quantity=item.quantity,
             unit_price=item.unit_price,
             total_price=item.unit_price * item.quantity,
@@ -47,18 +47,22 @@ class CartItemService:
         )
         if existing_item:
             new_quantity = existing_item.quantity + item_data.quantity
-            upd_item = await self.repository.update_quantity(
-                existing_item.id, new_quantity
+            upd_item = await self.repository.update(
+                existing_item.id,
+                CartItemPatchUpdate(quantity=new_quantity).model_dump()
             )
-            return await self._to_cart_item_read(upd_item)
+            return self._to_cart_item_read(upd_item)
 
         meal = await self.meal_service.get_meal(item_data.meal_id)
         item_dict = item_data.model_dump()
-        item_dict["cart_id"] = cart.id
-        item_dict["unit_price"] = meal.unit_price
-
+        item_dict.update({
+            "cart_id": cart.id,
+            "unit_price": meal.unit_price,
+            "meal_name": meal.name
+        })
         item = await self.repository.create(item_dict)
-        return await self._to_cart_item_read(item)
+
+        return self._to_cart_item_read(item)
 
     async def get_cart_items(self, user_id: int) -> list[CartItemRead]:
         cart = await self.cart_service.get_cart_by_user_id(user_id)
@@ -66,7 +70,7 @@ class CartItemService:
             raise CartNotFoundError(user_id)
 
         items = await self.repository.get_all_by_cart_id(cart.id)
-        return [await self._to_cart_item_read(item) for item in items]
+        return [self._to_cart_item_read(item) for item in items]
 
     async def get_cart_item(self, user_id: int, item_id: int) -> CartItemRead:
         cart = await self.cart_service.get_cart_by_user_id(user_id)
@@ -77,12 +81,19 @@ class CartItemService:
         if item is None or item.cart_id != cart.id:
             raise CartItemNotFoundError(item_id)
 
-        return await self._to_cart_item_read(item)
+        return self._to_cart_item_read(item)
 
     async def update_cart_item(
-        self, user_id: int, item_id: int, item_data: CartItemPatchUpdate
+        self,
+        user_id: int,
+        item_id: int,
+        item_data: CartItemPutUpdate | CartItemPatchUpdate,
+        is_partial: bool = False
     ) -> CartItemRead:
-        new_data = item_data.model_dump(exclude_unset=True)
+        new_data = (
+            item_data.model_dump(exclude_unset=True) if is_partial
+            else item_data.model_dump()
+        )
 
         if not new_data:
             raise NoCartItemUpdateDataError()
@@ -96,7 +107,7 @@ class CartItemService:
             raise CartItemNotFoundError(item_id)
 
         upd_item = await self.repository.update(item_id, new_data)
-        return await self._to_cart_item_read(upd_item)
+        return self._to_cart_item_read(upd_item)
 
     async def remove_item_from_cart(self, user_id: int, item_id: int) -> None:
         cart = await self.cart_service.get_cart_by_user_id(user_id)
