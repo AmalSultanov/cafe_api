07ec2644bd4fb9eef82ca8a2120c27d3a1aba422
AmalSultanov <pogboom1014@gmail.com>
2025-07-27 23:15:39 +0500

    Add CartPatchUpdate schema

--- src/schemas/cart.py
+++ src/schemas/cart.py
@@ -1,17 +1,27 @@
 from datetime import datetime
+from decimal import Decimal
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class CartBase(BaseModel):
     user_id: int
+
+    model_config = ConfigDict(
+        json_encoders={Decimal: lambda v: format(v, "f")}
+    )
 
 
 class CartCreate(CartBase):
     pass
 
 
+class CartPatchUpdate(BaseModel):
+    total_price: Decimal | None = None
+
+
 class CartRead(CartBase):
     id: int
+    total_price: Decimal | None
     created_at: datetime
     model_config = {"from_attributes": True}
