5c99d753e40f14a65b2a9bbd385ff11792b727e8
AmalSultanov <pogboom1014@gmail.com>
2025-07-27 23:28:01 +0500

    Add update_cart service method

--- src/services/cart/interface.py
+++ src/services/cart/interface.py
@@ -1,11 +1,15 @@
 from typing import Protocol
 
-from src.schemas.cart import CartRead
+from src.schemas.cart import CartRead, CartPatchUpdate
 
 
 class ICartService(Protocol):
     async def create_cart(self, user_id: int) -> CartRead: ...
 
     async def get_cart_by_user_id(self, user_id: int) -> CartRead: ...
+
+    async def update_cart(
+        self, user_id: int, cart_data: CartPatchUpdate
+    ) -> CartRead: ...
 
     async def delete_cart_by_user_id(self, user_id: int) -> None: ...
--- src/services/cart/service.py
+++ src/services/cart/service.py
@@ -1,6 +1,6 @@
 from src.exceptions.cart import CartAlreadyExistsError, CartNotFoundError
 from src.repositories.cart.interface import ICartRepository
-from src.schemas.cart import CartRead
+from src.schemas.cart import CartRead, CartPatchUpdate
 
 
 class CartService:
@@ -20,6 +20,19 @@ class CartService:
         if not cart:
             raise CartNotFoundError(user_id)
         return CartRead.model_validate(cart)
+
+    async def update_cart(
+        self, user_id: int, cart_data: CartPatchUpdate
+    ) -> CartRead:
+        cart = await self.repository.get_by_user_id(user_id)
+
+        if not cart:
+            raise CartNotFoundError(user_id)
+
+        upd_cart = self.repository.update(
+            user_id, cart_data.model_dump(exclude_unset=True)
+        )
+        return CartRead.model_validate(upd_cart)
 
     async def delete_cart_by_user_id(self, user_id: int) -> None:
         cart = await self.repository.get_by_user_id(user_id)
