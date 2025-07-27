7ca7afd817ceff4507be454946d012f020c25e39
AmalSultanov <pogboom1014@gmail.com>
2025-07-28 00:08:30 +0500

    Add update repo method

--- src/repositories/cart/interface.py
+++ src/repositories/cart/interface.py
@@ -10,5 +10,9 @@ class ICartRepository(Protocol):
     async def get_by_user_id(self, user_id: int) -> CartModel | None:
         ...
 
+    async def update(
+        self, user_id: int, cart_data: dict[str, str]
+    ) -> CartModel | None: ...
+
     async def delete(self, cart_id: int) -> None:
         ...
--- src/repositories/cart/repository.py
+++ src/repositories/cart/repository.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from sqlalchemy import delete
+from sqlalchemy import delete, update
 
 from src.models.cart import CartModel
 
@@ -23,6 +23,17 @@ class CartRepository:
             select(CartModel).where(CartModel.user_id == user_id)
         )
         return result.scalar_one_or_none()
+
+    async def update(
+        self, user_id: int, cart_data: dict[str, str]
+    ) -> CartModel | None:
+        await self.db.execute(
+            update(CartModel)
+            .where(CartModel.user_id == user_id)
+            .values(**cart_data)
+        )
+        await self.db.commit()
+        return await self.get_by_user_id(user_id)
 
     async def delete(self, user_id: int) -> None:
         await self.db.execute(
