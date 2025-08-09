2fdf9cd344b28110b6ebf5408a49c1078d181a11
AmalSultanov <pogboom1014@gmail.com>
2025-08-09 20:30:12 +0500

    Add DI for orders

--- src/core/dependencies/order.py
+++ src/core/dependencies/order.py
@@ -3,11 +3,15 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.database import get_session
 from src.core.dependencies.cart import get_cart_service
+from src.core.dependencies.cart_item import get_cart_item_service
+from src.core.dependencies.order_item import get_order_item_service
 from src.repositories.order.interface import IOrderRepository
 from src.repositories.order.repository import OrderRepository
 from src.services.cart.interface import ICartService
+from src.services.cart_item.interface import ICartItemService
 from src.services.order.interface import IOrderService
 from src.services.order.service import OrderService
+from src.services.order_item.interface import IOrderItemService
 
 
 def get_order_repo(
@@ -18,6 +22,10 @@ def get_order_repo(
 
 def get_order_service(
     repository: IOrderRepository = Depends(get_order_repo),
-    cart_service: ICartService = Depends(get_cart_service)
+    cart_service: ICartService = Depends(get_cart_service),
+    cart_item_service: ICartItemService = Depends(get_cart_item_service),
+    order_item_service: IOrderItemService = Depends(get_order_item_service)
 ) -> IOrderService:
-    return OrderService(repository, cart_service)
+    return OrderService(
+        repository, cart_service, cart_item_service, order_item_service
+    )
