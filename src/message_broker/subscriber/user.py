9729254ee6ed8c640c851dc3474f7a846c5b2a28
AmalSultanov <pogboom1014@gmail.com>
2025-08-10 15:52:52 +0500

    Add logging for UserCreatedEvent consumer

--- src/message_broker/subscriber/user.py
+++ src/message_broker/subscriber/user.py
@@ -1,4 +1,5 @@
 from src.core.database import async_session
+from src.core.logging import logger
 from src.exceptions.cart import CartAlreadyExistsError
 from src.message_broker.config import kafka_broker
 from src.message_broker.events.user import UserCreatedEvent
@@ -11,15 +12,34 @@ from src.services.cart.service import CartService
 
 @kafka_broker.subscriber(TOPIC_USER_CREATED, group_id="cart-create-service")
 async def create_cart_on_user_created(event: UserCreatedEvent):
-    print("Received user created event for cart service")
+    logger.info(
+        f"USER_SUBSCRIBER: Received UserCreatedEvent for user {event.user_id}"
+    )
     user_id = event.user_id
+
     async with async_session() as session:
         try:
+            logger.debug(
+                f"USER_SUBSCRIBER: Initializing cart repository and service for user {user_id}"
+            )
             repo = CartRepository(session)
             cart_service = CartService(repo)
+
+            logger.info(f"USER_SUBSCRIBER: Creating cart for user {user_id}")
             cart = await cart_service.create_cart(user_id)
-            print(cart)
+
+            logger.info(
+                f"USER_SUBSCRIBER: Cart was created for user "
+                f"{user_id} with cart ID: {cart.id}"
+            )
+
         except CartAlreadyExistsError as e:
-            print(f"Cart already exists: {e}")
+            logger.warning(
+                f"USER_SUBSCRIBER: Cart already exists for user {user_id}: {e}"
+            )
+            raise
         except Exception as e:
-            print(f"Unhandled exception in cart handler: {e}")
+            logger.error(
+                f"USER_SUBSCRIBER: Failed to create cart for user {user_id}: {e}"
+            )
+            raise
