d7b34b827b170099fa71036588ccbe9fd403e523
AmalSultanov <pogboom1014@gmail.com>
2025-08-10 15:56:25 +0500

    Add logging to kafka config

--- src/message_broker/config.py
+++ src/message_broker/config.py
@@ -1,10 +1,15 @@
 from faststream.kafka import KafkaBroker
 
 from src.core.config import get_settings
+from src.core.logging import logger
 
 settings = get_settings()
+logger.info(
+    f"Initializing Kafka broker with servers: {settings.kafka_bootstrap_servers}"
+)
 kafka_broker = KafkaBroker(settings.kafka_bootstrap_servers)
 
 
 async def get_kafka_broker() -> KafkaBroker:
+    logger.debug("Returning Kafka broker instance")
     return kafka_broker
