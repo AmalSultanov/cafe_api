81703cb71bc608bf5041f908cc772b2e7b2bb65d
AmalSultanov <pogboom1014@gmail.com>
2025-09-05 12:58:09 +0500

    Add cart repo interface

---
+++ src/repositories/interfaces/cart.py
@@ -0,0 +1,15 @@
+from typing import Protocol
+
+from src.models.cart import CartModel
+
+
+class ICartRepository(Protocol):
+    async def create(self, user_id: int) -> CartModel: ...
+
+    async def get_by_user_id(self, user_id: int) -> CartModel | None: ...
+
+    async def update(
+        self, user_id: int, cart_data: dict[str, str]
+    ) -> CartModel | None: ...
+
+    async def delete(self, cart_id: int) -> None: ...
